0a74b49217791e7e3a010f308fd1fc38d5032e3d
= <fedra.arg@gmail.com>
2022-10-25 16:51:51 -0300

    Server

--- src/server.js
+++ src/server.js
@@ -34,13 +34,13 @@ export default function server () {
       const pedrita = server.create('user', { name: 'Pedrita' })
       const mosty = server.create('user', { name: 'Mosty' })
 
-      const pedritaPost1 = server.create("post", { user: pedrita, title: "Getting lost in the forest 101" });
-      const pedritaPost2 = server.create("post", { user: pedrita, title: "Why I hate hypnos... And my dad" });
+      const pedritaPost1 = server.create("post", { user: pedrita, title: "Getting lost in the forest 101", body: 'I got lost once' });
+      const pedritaPost2 = server.create("post", { user: pedrita, title: "Why I hate hypnos... And my dad", body: 'Hypnos are ugly' });
       server.create("comment", { user: mosty, post: pedritaPost1, body: 'You suck, kid. 1v1 me.' });
       server.create("comment", { user: mosty, post: pedritaPost2, body: 'You suck, kid. 1v1 me.' });
 
-      const mostyPost1 = server.create("post", { user: mosty, title: "Water pokemon are the best!" });
-      const mostyPost2 = server.create("post", { user: mosty, title: "How I lost my bike to a kid without teeth" });
+      const mostyPost1 = server.create("post", { user: mosty, title: "Water pokemon are the best!", body: 'I like turtles' });
+      const mostyPost2 = server.create("post", { user: mosty, title: "How I lost my bike to a kid without teeth", body: 'I suck' });
       server.create("comment", { user: pedrita, post: mostyPost1, body: 'I live in a boring island' });
       server.create("comment", { user: mosty, post: mostyPost2, body: 'I comment in my own post because I am awesome!' });
     },
